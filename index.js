a93990371809c506549cd7419f9be777b6221dce
md ashif reza <76996887+md-786910@users.noreply.github.com>
2021-07-07 11:14:58 +0530

    update localhost

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:3000/");
+const socket = io("https://romantic-ptolemy-bf04b2.netlify.app/");
 
 function Appendposition(pos, data) {
     let element = document.createElement("div");
